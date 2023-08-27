9d020c1af29ec243fa59725ad3d30ae7d547c761
Pavlo2005 <savyukpavlo8@gmail.com>
2023-08-27 16:35:46 +0200

    commit task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+import { getRandomHexColor } from "./template/get-random-color";
 
+const element = {
+    body: document.body,
+    buttonStart: document.querySelector('.js-button-start'),
+    buttonStop: document.querySelector('.js-button-stop')
+}
+
+function changeColor() {
+    element.body.style.backgroundColor = getRandomHexColor();
+}
+
+let intervaId = null;
+
+element.buttonStart.addEventListener('click', trackingClickStart);
+
+function trackingClickStart() {
+    changeColor();
+
+    element.buttonStart.disabled = true;
+    element.buttonStop.disabled = false;
+
+    intervaId = setInterval(changeColor, 1000);
+}
+
+element.buttonStop.addEventListener('click', trackingClickStop);
+
+function trackingClickStop() {
+    element.buttonStart.disabled = false;
+    element.buttonStop.disabled = true;
+
+    clearInterval(intervaId)
+}
