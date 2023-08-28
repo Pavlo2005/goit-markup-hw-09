39f9b93af8c27287715a9c277ad83f3968f53a46
Pavlo2005 <savyukpavlo8@gmail.com>
2023-08-28 16:29:11 +0200

    commit task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      res({ position, delay });
+    } else {
+      rej({ position, delay });
+    }
+  })
+
+}
+
+const element = {
+  delay: document.querySelector('.js-delay'),
+  step: document.querySelector('.js-step'),
+  amount: document.querySelector('.js-amount'),
+  submit: document.querySelector('.js-submit')
+}
+
+element.submit.addEventListener('click', handlerClick);
+
+function handlerClick(evt) {
+
+  evt.preventDefault();
+
+  for (let i = 0; i < parseInt(element.amount.value); i += 1){
+    setTimeout(() => {
+      createPromise(i + 1, parseInt(element.delay.value) + ((i) * parseInt(element.step.value)))
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }, parseInt(element.delay.value) + ((i) * parseInt(element.step.value)) )
   }
+
 }
